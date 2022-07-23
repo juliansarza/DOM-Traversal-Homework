f5ed7ab07ec237a271886f487a74470183a4c92b
Julian Sarza <julian2570@gmail.com>
2022-07-23 15:27:18 -0700

    second commit

---
+++ To Do List/script.js
@@ -0,0 +1,22 @@
+// JavaScript variables
+
+const taskArray = [];
+
+// DOM variables
+
+//Title
+const title = document.getElementById("title");
+title.innerText = "Crazy Day To Do List";
+
+
+const bodyEl = document.getElementById("body");
+
+const buttonEl = document.createElement("button");
+buttonEl.innerHTML = "Bunnies so playful";
+
+
+
+
+
+
+
